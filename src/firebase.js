c7ad9868f248c14c24fc8750bd18b8b5d80bc504
annajolly <jolly.anna@ymail.com>
2020-03-22 12:58:25 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import "firebase/auth";
+import "firebase/database"
+import "firebase/storage"
+
+
+var config = {
+  apiKey: "AIzaSyBgi2Yj5MKMMt6SWm3cDZPbDQzlMQ1_UUo",
+  authDomain: "react-slack-clone-a4030.firebaseapp.com",
+  databaseURL: "https://react-slack-clone-a4030.firebaseio.com",
+  projectId: "react-slack-clone-a4030",
+  storageBucket: "react-slack-clone-a4030.appspot.com",
+  messagingSenderId: "1047846985812",
+  appId: "1:1047846985812:web:d3cc5f0158af8f05"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
+export default firebase;
